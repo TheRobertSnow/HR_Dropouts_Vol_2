5990180c20d0b084b7343e3012e9ff38fbdfd24e
Dinkus <robertsnaer901@gmail.com>
2020-05-04 23:03:58 +0000

    mangaged to fix link to login!

--- captain_console/CaptainConsole/urls.py
+++ captain_console/CaptainConsole/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('', views.login, name='login'),
+    path('home', views.home, name='home'),
+    path('login', views.login, name='login'),
 ]

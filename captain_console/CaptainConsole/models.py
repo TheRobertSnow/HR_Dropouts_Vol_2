c0586bb86cb8f0584583fa20a0701994648d339d
Dinkus <robertsnaer901@gmail.com>
2020-05-05 18:47:33 +0000

    Added classes to models.py

--- captain_console/CaptainConsole/models.py
+++ captain_console/CaptainConsole/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Users(models.Model):
+    pass
+
+
+class Products(models.Model):
+    pass
+
+
+class Reviews(models.Model):
+    pass
+
+
+class Orders(models.Model):
+    pass
+
+
+class SearchHistory(models.Model):
+    pass
+
+
+class ShoppingChart(models.Model):
+    pass

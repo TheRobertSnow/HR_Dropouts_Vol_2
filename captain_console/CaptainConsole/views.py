ee059327891cae1755ddede75cec74f034771487
Dinkus <robertsnaer901@gmail.com>
2020-05-06 11:00:08 +0000

    Added content to home.html

--- captain_console/CaptainConsole/views.py
+++ captain_console/CaptainConsole/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
+products = [
+    {'name':'PlayStation2', 'price': 19.99},
+    {'name':'wii', 'price': 3.99},
+]
 # Create your views here.
 def home(request):
-    return render(request, 'CaptainConsole/home.html')
+    return render(request, 'CaptainConsole/home.html', context={ 'products': products })
 
 def login(request):
     return render(request, 'CaptainConsole/login.html')

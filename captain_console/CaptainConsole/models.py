7f99d5d3c3be6456e71e9fb230f86d809f4350a3
Dinkus <robertsnaer901@gmail.com>
2020-05-06 14:08:28 +0000

    Added fixes to model variables

--- captain_console/CaptainConsole/models.py
+++ captain_console/CaptainConsole/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-    ID = models.IntegerField
+    ID = models.IntegerField()
     password = models.CharField(max_length = 255) # passa hash
-    avatar = models.IntegerField # pic within the database ?? idunno
+    avatar = models.IntegerField() # pic within the database ?? idunno
     nickname = models.CharField(max_length = 255)
     email = models.CharField(max_length = 255)
     # admin = models.IntegerField(max_length = 255)
@@ -12,28 +12,29 @@ class Users(models.Model):
 
 
 class Products(models.Model):
-    ID = models.IntegerField
+    ID = models.IntegerField()
     name = models.CharField(max_length = 255)
+    price = models.FloatField()
     description = models.CharField(max_length = 999)
     manufacturer = models.CharField(max_length = 255)
-    photoID = models.IntegerField
+    photoID = models.IntegerField()
 
 
 class Reviews(models.Model):
-    ID = models.IntegerField
-    userID = models.IntegerField
-    productID = models.IntegerField
-    rating = models.IntegerField
+    ID = models.IntegerField()
+    userID = models.IntegerField()
+    productID = models.IntegerField()
+    rating = models.IntegerField()
     review = models.CharField(max_length = 999) # meira ? idunno
 
 
 class Orders(models.Model):
-    ID = models.IntegerField
-    userId = models.IntegerField
-    shoppingCartID = models.IntegerField
+    ID = models.IntegerField()
+    userId = models.IntegerField()
+    shoppingCartID = models.IntegerField()
     name = models.CharField(max_length = 255)
-    price = models.IntegerField
-    amount = models.IntegerField
+    price = models.FloatField()
+    amount = models.FloatField()
     additionalInfo = models.CharField(max_length = 999, blank= True)
     address = models.CharField(max_length = 255)
     zipcode = models.CharField(max_length = 255)
@@ -43,13 +44,13 @@ class Orders(models.Model):
 
 
 class SearchHistory(models.Model):
-    ID = models.IntegerField
-    userID = models.IntegerField
-    keywordIDs = models.IntegerField
+    ID = models.IntegerField()
+    userID = models.IntegerField()
+    keywordIDs = models.IntegerField()
     search = models.CharField(max_length = 255) #sentance
     datetime = models.DateTimeField(max_length=255)
 
 class ShoppingChart(models.Model):
-    ID = models.IntegerField
-    userID = models.IntegerField
-    productIDs = models.IntegerField
+    ID = models.IntegerField()
+    userID = models.IntegerField()
+    productIDs = models.IntegerField()

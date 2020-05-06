c1d317d1978dc97e1f399bbe4d820ab792405e64
Daníel Þór Gestsson <danni539953@gmail.com>
2020-05-06 12:02:39 +0000

    danni product box king

--- captain_console/CaptainConsole/views.py
+++ captain_console/CaptainConsole/views.py
@@ -2,6 +2,17 @@ from django.shortcuts import render
 products = [
     {'name':'PlayStation2', 'price': 19.99},
     {'name':'wii', 'price': 3.99},
+    {'name':'Xbox360', 'price': 5.99},
+    {'name':'He on xgames', 'price': 8.49},
+    {'name':'wii', 'price': 3.99},
+    {'name':'wii', 'price': 3.99},
+    {'name':'wii', 'price': 3.99},
+    {'name':'wii', 'price': 3.99},
+    {'name':'wii', 'price': 3.99},
+    {'name':'wii', 'price': 3.99},
+    {'name':'wii', 'price': 3.99},
+    {'name':'wii', 'price': 3.99},
+    {'name':'wii', 'price': 3.99},
 ]
 # Create your views here.
 def home(request):

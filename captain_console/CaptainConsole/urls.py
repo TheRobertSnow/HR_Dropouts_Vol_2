bcb8217aa4fdd769132c7a5c5c36b1d7c97d0f83
Daníel Þór Gestsson <danni539953@gmail.com>
2020-05-06 16:02:19 +0000

    bætt við product page fyrir oll product

--- captain_console/CaptainConsole/urls.py
+++ captain_console/CaptainConsole/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
     path('login', views.login, name='login'),
+    path('products/<int:id>', views.get_product_by_id, name='product_details'),
 ]
--- captain_console/CaptainConsole/views.py
+++ captain_console/CaptainConsole/views.py
@@ -1,28 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+
 products = [
-    {'image':'1.jpg', 'name':'PlayStation1', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
-    {'image':'3.jpg', 'name':'PlayStation3', 'price': 9.99},
-    {'image':'4.jpg', 'name':'PlayStation4', 'price': 8.99},
-    {'image':'5.jpg', 'name':'Xbox 360', 'price': 7.99},
-    {'image':'6.jpg', 'name':'Xbox One', 'price': 6.99},
-    {'image':'7.jpg', 'name':'Sega Genesis', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
-    {'image':'2.jpg', 'name':'PlayStation2', 'price': 19.99},
+    {'id':'0','image':'1.jpg', 'name':'PlayStation1', 'price': 19.99, 'description':'This is some long as description of the item'},
+    {'id':'1','image':'2.jpg', 'name':'PlayStation2', 'price': 19.99, 'description':'This is some long as description of the item'},
+    {'id':'2','image':'3.jpg', 'name':'PlayStation3', 'price': 9.99, 'description':'This is some long as description of the item'},
+    {'id':'3','image':'4.jpg', 'name':'PlayStation4', 'price': 8.99, 'description':'This is some long as description of the item'},
+    {'id':'4','image':'5.jpg', 'name':'Xbox 360', 'price': 7.99, 'description':'This is some long as description of the item'},
+    {'id':'5','image':'6.jpg', 'name':'Xbox One', 'price': 6.99, 'description':'This is some long as description of the item'},
+    {'id':'6','image':'7.jpg', 'name':'Sega Genesis', 'price': 19.99, 'description':'This is some long as description of the item'},
+    {'id':'7','image':'2.jpg', 'name':'PlayStation2', 'price': 19.99, 'description':'This is some long as description of the item'},
+    {'id':'8','image':'2.jpg', 'name':'PlayStation2', 'price': 19.99, 'description':'This is some long as description of the item'},
+    {'id':'9','image':'2.jpg', 'name':'PlayStation2', 'price': 19.99, 'description':'This is some long as description of the item'},
+    {'id':'10','image':'2.jpg', 'name':'PlayStation2', 'price': 19.99, 'description':'This is some long as description of the item'},
 ]
 # Create your views here.
 def home(request):
@@ -30,3 +20,6 @@ def home(request):
 
 def login(request):
     return render(request, 'CaptainConsole/login.html')
+
+def get_product_by_id(request, id):
+    return render(request, 'CaptainConsole/product.html', context={ 'product': products[id] })

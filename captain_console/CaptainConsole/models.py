96e31e3e0fc99cfe03c1903ac5e5c5dcf9c64b9b
Daníel Þór Gestsson <danni539953@gmail.com>
2020-05-07 17:09:28 +0000

    Bæng

--- captain_console/CaptainConsole/models.py
+++ captain_console/CaptainConsole/models.py
@@ -2,48 +2,47 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-    password = models.CharField(max_length = 255) # passa hash
-    avatar = models.IntegerField() # pic within the database ?? idunno
-    nickname = models.CharField(max_length = 255)
-    email = models.CharField(max_length = 255)
+    password = models.CharField(max_length=255) # passa hash
+    avatar = models.CharField(max_length=999) # pic within the database ?? idunno
+    nickname = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
     admin = models.BooleanField()
 
-
 class Products(models.Model):
-    name = models.CharField(max_length = 255)
+    name = models.CharField(max_length=255)
     price = models.FloatField()
-    description = models.CharField(max_length = 999)
-    manufacturer = models.CharField(max_length = 255)
-    photoID = models.IntegerField()
+    description = models.CharField(max_length=999)
+    manufacturer = models.CharField(max_length=255)
 
+class ProductImages(models.Model):
+    imageFileName = models.CharField(max_length=999)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE, blank=True, null=True)
 
 class Reviews(models.Model):
-    userID = models.IntegerField()
-    productID = models.IntegerField()
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, blank=True, null=True)
+    product = models.ForeignKey(Products, on_delete=models.CASCADE, blank=True, null=True)
     rating = models.IntegerField()
-    review = models.CharField(max_length = 999) # meira ? idunno
+    reviewText = models.CharField(max_length=999) # meira ? idunno
 
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, blank=True, null=True)
+    productIDs = models.CharField(max_length=999)
+    productAmount = models.CharField(max_length=999)
 
 class Orders(models.Model):
-    userId = models.IntegerField()
-    shoppingCartID = models.IntegerField()
-    name = models.CharField(max_length = 255)
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, blank=True, null=True)
+    shoppingCart = models.ForeignKey(ShoppingCart, on_delete=models.CASCADE, blank=True, null=True)
+    name = models.CharField(max_length=255)
     price = models.FloatField()
     amount = models.FloatField()
-    additionalInfo = models.CharField(max_length = 999, blank= True)
-    address = models.CharField(max_length = 255)
-    zipcode = models.CharField(max_length = 255)
-    city = models.CharField(max_length = 255)
-    country = models.CharField(max_length = 255)
-
-
+    additionalInfo = models.CharField(max_length=999, blank= True)
+    address = models.CharField(max_length=255)
+    zipcode = models.CharField(max_length=255)
+    city = models.CharField(max_length=255)
+    country = models.CharField(max_length=255)
 
 class SearchHistory(models.Model):
-    userID = models.IntegerField()
-    keywordIDs = models.IntegerField()
-    search = models.CharField(max_length = 255) #sentance
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, blank=True, null=True)
+    keywordIDs = models.CharField(max_length=999)
+    searchQuery = models.CharField(max_length=255) #sentance
     datetime = models.DateTimeField(max_length=255)
-
-class ShoppingChart(models.Model):
-    userID = models.IntegerField()
-    productIDs = models.IntegerField()

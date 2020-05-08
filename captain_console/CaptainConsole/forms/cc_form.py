f3b9d25226ffa0364d16eb2fc39d12c05bbf14cd
unknown <bergurmg@gmail.com>
2020-05-08 12:36:52 +0000

    User form

--- captain_console/CaptainConsole/forms/cc_form.py
+++ captain_console/CaptainConsole/forms/cc_form.py
@@ -1,7 +1,7 @@
 from django.forms import ModelForm
 from django.forms import widgets
 from django import forms
-from CaptainConsole.models import Products
+from CaptainConsole.models import Products, Users
 
 
 class ProductCreateForm(ModelForm):
@@ -16,3 +16,18 @@ class ProductCreateForm(ModelForm):
             'description': widgets.TextInput(attrs={'class': 'form-control'}),
             'manufacturer': widgets.TextInput(attrs={'class': 'form-control'}),
         }
+
+class UserCreateForm(ModelForm):
+    image = forms.CharField(required=True, widget=forms.TextInput({'class': 'form-control'}))
+    class Meta:
+        model = Products
+        exclude = ['id']
+        widgets = { 'password': widgets.TextInput(attrs={'class': 'form-control' }),
+                    'avatar': widgets.NumberInput(attrs={'class': 'form-control' }),
+                    'nickname': widgets.TextInput(attrs={'class': 'form-control' }),
+                    'email': widgets.TextInput(attrs={'class': 'form-control' }),
+                    'admin': widgets.CheckboxInput(attrs={'class': 'form-control'}),
+        }
+
+
+

4a8263cf2f1876562287d562942c84f2cb3f68db
Daníel Þór Gestsson <danni539953@gmail.com>
2020-05-08 14:08:10 +0000

    fijdhuf

--- captain_console/CaptainConsole/models.py
+++ captain_console/CaptainConsole/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Users(models.Model):
-    password = models.CharField(max_length=255) # passa hash
-    avatar = models.CharField(max_length=999) # pic within the database ?? idunno
+    password = models.CharField(max_length=255)
+    avatar = models.CharField(max_length=999)
     nickname = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     admin = models.BooleanField()
@@ -25,7 +25,9 @@ class Reviews(models.Model):
     user = models.ForeignKey(Users, on_delete=models.CASCADE, blank=True, null=True)
     product = models.ForeignKey(Products, on_delete=models.CASCADE, blank=True, null=True)
     rating = models.IntegerField()
-    reviewText = models.CharField(max_length=999) # meira ? idunno
+    reviewTitle = models.CharField(max_length=255)
+    reviewText = models.CharField(max_length=999)
+    datetime = models.DateTimeField(max_length=255)
 
 class ShoppingCart(models.Model):
     user = models.ForeignKey(Users, on_delete=models.CASCADE, blank=True, null=True)
@@ -47,5 +49,5 @@ class Orders(models.Model):
 class SearchHistory(models.Model):
     user = models.ForeignKey(Users, on_delete=models.CASCADE, blank=True, null=True)
     keywordIDs = models.CharField(max_length=999)
-    searchQuery = models.CharField(max_length=255) #sentance
+    searchQuery = models.CharField(max_length=255)
     datetime = models.DateTimeField(max_length=255)

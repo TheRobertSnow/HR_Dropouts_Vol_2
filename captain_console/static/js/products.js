3aa6241aa9402887b396c248eb1cb51a74a65f1d
Dinkus <robertsnaer901@gmail.com>
2020-05-12 14:59:11 +0000

    fixed display bug

--- captain_console/static/js/products.js
+++ captain_console/static/js/products.js
@@ -13,7 +13,9 @@ $(document).ready(function () {
                 var newHtml = resp.data.map(d => {
                     return `<div class=product-container>
                                 <a href="/products/${d.id}">
-                                    <img class="product-mainImage" src="${d.mainImageLink}" alt="">
+                                    <div class="product-mainImage">
+                                        <img src="${d.mainImageLink}" alt="">
+                                    </div>
                                     <h4 class="product-name">${d.name}</h4>
                                     <p class="product-price">${d.price} kr.</p>
                                 </a>
